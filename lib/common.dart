7ce3cabb231c472c510892506201a63ec4b27cfc
Sunha.Park <sunha321@gmail.com>
2022-08-04 02:04:53 +0900

    test

--- lib/common.dart
+++ lib/common.dart
@@ -1,19 +1,27 @@
 library common;
 
 import 'dart:async';
+import 'dart:convert';
 import 'dart:math';
 
 // packageImport
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 // preset
 // import 'package:first/preset/router.dart' as ROUTER;
 import 'package:first/preset/router.dart' as ROUTER;
 
+// service
+part 'service/fetch_lotto.dart';
+
+// model
+part 'model/lotto.dart';
+part 'model/lotto_info.dart';
+
 // part
 part 'view/home.dart';
-part 'model/lotto.dart';
 part 'global.dart';
 
 part 'extension.dart';
--- lib/service/fetch_lotto.dart
+++ lib/service/fetch_lotto.dart
@@ -4,8 +4,8 @@ Future<LottoInfo> fetchLotto() async {
   String url =
       'https://cors-anywhere.herokuapp.com/https://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo=931';
 
-  // String url =
-  //     'https://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo=931';
+  String url_2 =
+      'https://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo=931';
   final response = await http.get(Uri.parse(url));
 
   if (response.statusCode == 200) {
